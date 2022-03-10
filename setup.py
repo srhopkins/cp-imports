c9d265051dcb39bd77c422056515c38f827bebad
Steven Hopkins <steve@cloudposse.com>
2022-03-10 14:55:40 -0700

    fixing entry point

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='cp-imports',
-    version='0.0.2',
+    version='0.0.3',
     author='Steven Hopkins',
     author_email='srhopkins@gmail.com',
     description='Tools to track Cloud Posse imports in stack configs',
@@ -13,11 +13,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/srhopkins/cp-imports',
     license='MIT',
-    packages=['cp-imports'],
+    packages=['imports'],
     install_requires=[],
     entry_points = {
         'console_scripts': [
-            'cp-test = package.module:main',
+            'cp-imports=imports:main',
             ],
     }
 )
